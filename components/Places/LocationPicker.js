201c00dd8b8b3b1efe38498475d1d5eded2078bd
eunji Lee <eunjitech@gmail.com>
2023-05-23 16:06:03 +0900

    12-201: 사용자 위치 가져오기

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,10 +1,46 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
+
 import { Colors } from "../../constants/colors";
 import OutlinedButton from "../ui/OutlinedButton";
 
 export default function LocationPicker() {
-  function getLocationHandler() {}
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted; //부여되면 true
+    }
+
+    if (locationPermissionInformation.stauts === PermissionStatus.DENIED) {
+      Alert.alert("권한없음", "위치 권한이 없습니다.");
+      return false;
+    }
+
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      //권한이 없으면 return
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    console.log("location", location);
+  }
   function pickOnMapHandler() {}
   return (
     <View>
